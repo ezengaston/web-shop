1d54bafa889719595bafcb46b6677d17ab810487
Ezen <82026690+ezengaston@users.noreply.github.com>
2021-12-01 17:26:51 +0000

    Make It Better

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { Products, Navbar } from "./components";
+
 function App() {
   return (
     <div>
-      <h1>Store</h1>
+      <Navbar />
+      <Products />
     </div>
   );
 }
