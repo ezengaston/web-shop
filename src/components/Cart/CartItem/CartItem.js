894cdf6a59774063fcc68f594a6f8a87a565e216
Ezen <82026690+ezengaston@users.noreply.github.com>
2021-12-02 19:30:54 +0000

    Make It Better

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -1,9 +1,45 @@
 import React from "react";
+import {
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core";
 
-export default function CartItem() {
+import useStyles from "./styles";
+
+export default function CartItem({ item }) {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Cart Item</h1>
-    </div>
+    <Card>
+      <CardMedia
+        image={item.image.url}
+        alt={item.name}
+        className={classes.media}
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button type="button" size="small">
+            -
+          </Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">
+            +
+          </Button>
+        </div>
+        <Button variant="contained" type="button" color="secondary">
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
